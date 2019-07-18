22bb94d0f659f5ac25fa7094c841e5b5dd22cf66
Nemanja Paroski <paroskidev@gmail.com>
2019-07-18 20:19:14 +0200

    Formated message that is being emailed to correct format.

--- web/php/sendMail.php
+++ web/php/sendMail.php
@@ -2,4 +2,5 @@
   $input = file_get_contents('php://input');
   $data = json_decode($input);
   $header = "From: " . $data->name . ", E-mail: " . $data->email;
-  mail("info@koddzona.rs","Poruka sa veb stranice",$data->msg,$header);
+  $msg = "Ime: ".$data->name."\n"."Email: ". $data->email ."\n\n"."Poruka: \n\n".$data->msg;
+  mail("info@koddzona.rs","Poruka sa veb stranice",$msg,$header,$data->email);
